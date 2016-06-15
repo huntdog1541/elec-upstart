fc7c5d951116ba8d62a3bc999a5adff5e130f452
David Hunt <dhunt@Davids-Mac.local>
2016-06-14 19:55:15 -0500

    electron update

---
+++ src/app.js
@@ -0,0 +1,14 @@
+var app = require('app'), ipc = require('ipc'),
+ BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+	mainWindow = new BrowserWindow({
+		width: 1024,
+		height: 768
+	});
+
+	mainWindow.loadUrl('file://' + __dirname + '/windows/main/main.html');
+	mainWindow.openDevTools();
+});
